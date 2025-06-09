871ba423b51a5d571615ecc348f24cc2f103543c
IPYURA <0207ehdrjs@naver.com>
2025-06-09 18:01:06 +0900

    pracice

--- hello.py
+++ hello.py
@@ -1,3 +1,31 @@
-#start
+# reversed_number = user_input[::-1]
+# print(reversed_number);
+
+user_input = input('사용자 입력: ')
+
+# def practice(text):
+#     first_letter = text[0:1]
+#     second_letter = text[2:3]
+#     rest_letter = text[-3:]
+
+#     print(first_letter)
+#     print(second_letter)
+#     print(rest_letter)
+
+def practice():
+    first_letter = text[0:1]
+    second_letter = text[2:3]
+    rest_letter = text[-3:]
+
+    print(first_letter)
+    print(second_letter)
+    print(rest_letter)
+
+
+
+
+
+practice(user_input)
+
 
 
